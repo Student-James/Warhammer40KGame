a4ab23343d5ec2add5da49a1f08649354d56c839
SJ <james.head@mac.com>
2020-10-26 22:56:19 +0000

    Adding some error catching methods and a new start class.

--- src/W40k/MovePhase.java
+++ src/W40k/MovePhase.java
@@ -6,33 +6,43 @@ import java.util.Scanner;
 public class MovePhase {
 
     int diceRoll;
-    ArrayList<Unit> myArmy;
+    ArrayList<Unit> myArmy =new ArrayList<Unit>();
 
 
-    public void movePhase(ArrayList<Unit> myArmy,int diceRoll){
-        this.myArmy=new ArrayList<Unit>();
+
+
+    public void movePhase(ArrayList<Unit> myArmy){
+
+
+        DiceRoll diceRoll = new DiceRoll();
+        int movePhaseDice = diceRoll.DiceRoll();
+        Scanner userInput = new Scanner(System.in);
 
 
         System.out.println("*************************************************************************************");
         System.out.println("Which Unit would you like to move? Please enter the number of the unit: " );
         for(int n=0;n<myArmy.size();n++){
-            System.out.println( n +" "+myArmy.get(n).getName());
+            System.out.println( n +" "+myArmy.get(n).getName()); //Print the name of the army.
         }
         for(Unit u:myArmy){
-            Scanner userInput = new Scanner(System.in);
-            int userUnitNumber = userInput.nextInt();
+
+            int userUnitNumber = userInput.nextInt(); //User input to decide which unit they wish to move.
+            if (userUnitNumber>myArmy.size()||userUnitNumber<0){
+                System.out.println("The number you have entered is not a number of your unit. Please select a correct unit number. ");
+                movePhase(myArmy);// capturing a error if the user does not enter the the correct number.
+            }
             Unit tempUnit = myArmy.get(userUnitNumber);
             System.out.println("Do you wish to do a normal move(enter 0) or and advance(enter 1)?");
-            int userMoveIntension = userInput.nextInt();
+            int userMoveIntension = userInput.nextInt();//ask the user if they want to commit a move or a advance.
             if (userMoveIntension==0){
-                System.out.println("You can move this unit "+(tempUnit.getUnitMove())+"\"");
+                System.out.println("You can move this unit "+(tempUnit.getUnitMove())+"\"");// user has decided to move the unit, the code will tell them how far they can move their unit.
             }else if(userMoveIntension==1) {
-                System.out.println("You can move this unit " + (diceRoll + tempUnit.getUnitMove())+"\"");
+                System.out.println("You can move this unit " + (movePhaseDice + tempUnit.getUnitMove())+"\"");// user has decided that they will advance their units and have set charge to true so that they wont be able to attack when we reach the attack phase.
                 u.setCharge(true);
             }
             else{
                 System.out.println("You not entered the correct number. Please enter a 0 for move or a 1 for advance.");
-                movePhase(myArmy,diceRoll);
+                movePhase(myArmy);// capturing a error if the user does not enter the the correct number.
             }
             System.out.println("*************************************************************************************");
         }
